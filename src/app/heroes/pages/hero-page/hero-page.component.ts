9329ef245ae3f780cc75a14ca0eb72b94767116f
Edinson Aldaz <edinson.aldaz@gmail.com>
2024-06-17 12:16:17 -0500

    ver detalle del héroe

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Hero } from '../../interfaces/hero.interface';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'heroes-hero-page',
   templateUrl: './hero-page.component.html',
   styleUrl: './hero-page.component.css'
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
+
+  public hero?: Hero;
+
+  constructor(
+    private service: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  public ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({id}) => this.service.getHeroById(id))
+      )
+      .subscribe(hero => {
+        if (!hero) return this.router.navigateByUrl('heroes/list');
+
+        return this.hero = hero;
+      })
+  }
+
+  public goBack(): void {
+    this.router.navigateByUrl('heroes/list');
+  }
+
 
 }
