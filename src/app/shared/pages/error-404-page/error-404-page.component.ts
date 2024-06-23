5f99a9d0e055502a37a0f1b252e663d6bba7ddc1
Edinson Aldaz <edinson.aldaz@gmail.com>
2024-06-23 12:32:15 -0500

    Se elimina selector de componente página en 404-page

--- src/app/shared/pages/error-404-page/error-404-page.component.ts
+++ src/app/shared/pages/error-404-page/error-404-page.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-error-404-page',
   templateUrl: './error-404-page.component.html',
   styleUrl: './error-404-page.component.css'
 })
