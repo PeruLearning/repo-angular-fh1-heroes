e53803cff83cbda48f21d6ce41d4c0c1d12e7d34
Edinson Aldaz <edinson.aldaz@gmail.com>
2024-06-24 18:32:07 -0500

    se agrega selector a la página 404

--- src/app/shared/pages/error-404-page/error-404-page.component.ts
+++ src/app/shared/pages/error-404-page/error-404-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  selector: 'shared-404',
   templateUrl: './error-404-page.component.html',
   styleUrl: './error-404-page.component.css'
 })
